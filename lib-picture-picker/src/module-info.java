b85fcce91f13d58fababa68d49450f6012415cb4
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-06-27 14:18:45 -0400

    Format code.

--- lib-picture-picker/src/module-info.java
+++ lib-picture-picker/src/module-info.java
@@ -1,6 +1,6 @@
 module com.puttysoftware.picturepicker {
-	requires transitive java.desktop;
-	requires transitive com.puttysoftware.images;
-	
-	exports com.puttysoftware.picturepicker;
+    requires transitive java.desktop;
+    requires transitive com.puttysoftware.images;
+
+    exports com.puttysoftware.picturepicker;
 }
